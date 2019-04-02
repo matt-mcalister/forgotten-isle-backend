0bfb15deb09c3df1d519656f4e13b7856b4a890c
matt-mcalister <matt.mcalister93@gmail.com>
2019-04-02 12:06:37 -0400

    created seeds for games, destroys all data when run

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 
+Message.destroy_all
+ActiveGame.destroy_all
+Tile.destroy_all
+Game.destroy_all
+User.destroy_all
+
+Game.create(name: "Beginners only!", water_level: 1)
+Game.create(name: "Explorers Unite", water_level: 2)
+Game.create(name: "Let's do this", water_level: 3)
+Game.create(name: "Indiana Jones 4 life", water_level: 4)
